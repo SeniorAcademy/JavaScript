421eb55f2d6e2ec7d933ca0b9f0c3bb9ec5a696a
Senior Academy <151378391+SeniorAcademy@users.noreply.github.com>
2023-12-28 02:43:30 +0400

    Add files via upload

---
+++ JS-intro/variables/script.js
@@ -0,0 +1,71 @@
+// Değişkenler
+
+// var age;
+// console.log(age) // undefined
+
+// age = 20;
+// console.log(age) // 20
+
+// var number = 10;
+// var number2 = 15;
+
+// var number3 = number + number2;
+// console.log(number3)
+
+// var say;
+// say = 30;
+// console.log(say)
+
+// var fullname = 'Elvin';
+// // console.log(fullname)
+
+// var fullname = 'Huseyn';
+// console.log(fullname)
+
+//  Değişken Tanımlama Kuralları
+//  Değişken isimleri rakam ile başlayamaz.
+
+//  var 1yas; => hatalı
+//  var yas1; => geçerli
+//  var _yas; => geçerli
+
+//   Komut isimleriyle tanımlama yapılamaz. Örneğin if kelimesi dğeişken ismi olamaz.
+//   Büyük küçük harf duyarlılığı vardır.
+
+// var firstName = 'Huseyn';
+// var FirstName = 'Elvin';
+
+//     Farklı değişken isimleridir.
+//   Değişken isimlerinde türkçe karakter kullanmamalıyız.
+
+
+// VAR YAZILANI IF-DEN COLDE CAGIRMAQ VE DEYISMEK OLACAQ
+// LET VE CONST ISE CAGIRMAQ OLMAYACAQ ANCAQ GLOBALDAN
+
+
+
+///////////////  VAR
+
+var a = 100;
+var a = 200; // Isleyecek
+a = 300; // Isleyecek
+// console.log(a)
+
+
+//////////////// LET
+
+let b = 20;
+//let b = 30; // Error verecek
+//b = 30; // Isleyecek
+// console.log(b)
+
+
+////////////// CONST
+
+const c = 50;
+// const c = 60; // Error verecek
+//c = 70; // Error verecek
+// console.log(c)
+
+
+
