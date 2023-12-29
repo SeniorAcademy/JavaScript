bfe23c853c04fa63e9be562b2906d028578a1724
Senior Academy <151378391+SeniorAcademy@users.noreply.github.com>
2023-12-30 03:14:23 +0400

    Add files via upload

---
+++ JS-intro/Date and Time/script2.js
@@ -0,0 +1,37 @@
+
+// var dt = new Date();
+// console.log(dt);
+// console.log(dt.getMonth()+1);
+// console.log(dt.getDate());
+// console.log(dt.getFullYear());
+
+// var dtA = new Date('8/24/2010 14:50:10');
+// var dtB = new Date(2010,7,24,14,50,10)
+
+// console.log(dtA);
+// console.log(dtB);
+
+// var dtC = new Date('1/1/2000');
+// var dayOfMonth = dtC.getDate();
+// dtC.setDate(dayOfMonth-1);
+// console.log(dtC);
+
+
+
+
+// Task
+var start = new Date('1/1/2020');
+var end = new Date('1/1/2023');
+
+var milisecond = end - start;
+var saniye = milisecond / 1000;
+var dakika = saniye / 60;
+var saat = dakika / 60;
+var gun = saat / 24;
+
+console.log('milisecond :'+ milisecond)
+console.log('saniye : '+ saniye);
+console.log('dakika :'+ dakika);
+console.log('saat :' + saat);
+console.log('gun : '+ gun);
+
