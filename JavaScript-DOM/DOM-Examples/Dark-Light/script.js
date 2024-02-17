5fbdcf12479e4e3241bcfc468940ca7ee431ed00
Senior Academy <151378391+SeniorAcademy@users.noreply.github.com>
2024-02-17 19:13:52 +0400

    Update script.js

--- JavaScript-DOM/DOM-Examples/Dark-Light/script.js
+++ JavaScript-DOM/DOM-Examples/Dark-Light/script.js
@@ -1,30 +1,3 @@
-// const body = document.querySelector("body"),
-//   sidebar = body.querySelector("nav"),
-//   toggleBtn = body.querySelector(".toggle"),
-//   searchBtn = body.querySelector(".search-box"),
-//   modeSwitch = body.querySelector(".toggle-switch"),
-//   modeText = body.querySelector(".mode-text");
-
-// toggleBtn.addEventListener("click", () => {
-//   sidebar.classList.toggle("close");
-
-// });
-
-// searchBtn.addEventListener("click", () => {
-//   sidebar.classList.remove("close");
-// });
-
-// modeSwitch.addEventListener("click", () => {
-//   body.classList.toggle("dark");
-
-//   if (body.classList.contains("dark")) {
-//     modeText.innerText = "Dark Mode";
-//   }
-//    else {
-//     modeText.innerText = "Light Mode";
-//   }
-// });
-
 const body = document.querySelector("body"),
   sidebar = body.querySelector("nav"),
   toggleBtn = body.querySelector(".toggle"),
@@ -32,17 +5,9 @@ const body = document.querySelector("body"),
   modeSwitch = body.querySelector(".toggle-switch"),
   modeText = body.querySelector(".mode-text");
 
-// Local storage'dan kaydedilmiş moda bakarak sayfa yüklenirken modu ayarla
-window.addEventListener("DOMContentLoaded", () => {
-  const savedMode = localStorage.getItem("mode");
-  if (savedMode === "dark") {
-    body.classList.add("dark");
-    modeText.innerText = "Dark Mode";
-  }
-});
-
 toggleBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
+
 });
 
 searchBtn.addEventListener("click", () => {
@@ -54,9 +19,9 @@ modeSwitch.addEventListener("click", () => {
 
   if (body.classList.contains("dark")) {
     modeText.innerText = "Dark Mode";
-    localStorage.setItem("mode", "dark"); // Dark moda geçiş yapıldığında kaydet
-  } else {
+  }
+   else {
     modeText.innerText = "Light Mode";
-    localStorage.setItem("mode", "light"); // Light moda geçiş yapıldığında kaydet
   }
 });
+
